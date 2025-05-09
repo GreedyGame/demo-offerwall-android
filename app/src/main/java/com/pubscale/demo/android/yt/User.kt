4e81ba20f7a391a0a436b77e2323d448e48354a0
greedyraagava <raagava.prasadh@greedygame.com>
2025-05-09 17:34:02 +0530

    added comments on user class

--- app/src/main/java/com/pubscale/demo/android/yt/User.kt
+++ app/src/main/java/com/pubscale/demo/android/yt/User.kt
@@ -5,11 +5,14 @@ import android.content.SharedPreferences
 import androidx.core.content.edit
 import java.util.UUID
 
+//This is an helper class which will create an user id and persist it until the app data is cleared or the app is reinstalled
 class User(private val context: Context) {
 
     private val sharePreferences: SharedPreferences =
         context.getSharedPreferences("ps_demo", Context.MODE_PRIVATE)
 
+    //This method can be used to get an random auto generated user id which is persisted until the app data is cleared.
+    //Use this only if your app does not have a user id.
     fun getUserId(): String {
         var userId = sharePreferences.getString(USER_ID, "")
         if (userId.isNullOrEmpty()) {
